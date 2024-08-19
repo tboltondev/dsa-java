50450e0a1ca3c85f4bb9ed9ddd147c9b504ec97e
Tom Bolton <tbolton96@icloud.com>
2024-08-19 13:07:45 +0100

    add test for linear search

--- src/test/java/dev/tombolton/dsa/arraysAndStrings/examples/LinearSearchTest.java
+++ src/test/java/dev/tombolton/dsa/arraysAndStrings/examples/LinearSearchTest.java
@@ -6,6 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinearSearchTest {
     @Test
+    public void shouldReturnCorrectIndex() {
+        // Arrange
+        int[] arr = {10, 6, 3, 2};
+
+        // Act
+        int result = LinearSearch.search(arr, 3);
+
+        // Assert
+        assertEquals(result, 2);
+    }
+
+    @Test
     public void shouldReturnCorrectIndexForFirstMatch() {
         // Arrange
         int[] arr = {6, 4, 2, 0, 5, 9, 10, 5};
