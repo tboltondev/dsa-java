33903feea39086e31b86fc24e898528878929b41
Tom Bolton <tbolton96@icloud.com>
2024-08-19 14:21:36 +0100

    bubble sort implementation and test

---
+++ src/main/java/dev/tombolton/dsa/arraysAndStrings/examples/BubbleSort.java
@@ -0,0 +1,26 @@
+package dev.tombolton.dsa.arraysAndStrings.examples;
+
+public class BubbleSort {
+    public static int[] sort(int[] arr) {
+        boolean swapped;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j +  1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                return arr;
+            }
+        }
+
+        return arr;
+    }
+}
