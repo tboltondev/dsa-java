87b9bee7049e96c07cf189a9aecc7f59f4ad680c
Tom Bolton <tbolton96@icloud.com>
2024-08-21 10:42:49 +0100

    non duplicate hash table exercise and solution

---
+++ src/main/java/dev/tombolton/dsa/hashTables/exercises/nonDuplicateChar/QUESTION.md
@@ -0,0 +1,4 @@
+## Chapter 8 Exercise 4 (pg. 131)
+Write a function that returns the first non-duplicated character in a string. For example, the string `"minimum"` has
+two characters that only exist once - the `"n"` and the `"u"`, so your function should return the `"n"`, since it occurs
+first. The function should have an efficiency of $O(N)$.
---
+++ src/test/java/dev/tombolton/dsa/hashTables/exercises/nonDuplicateChar/NonDuplicateCharTest.java
@@ -0,0 +1,42 @@
+package dev.tombolton.dsa.hashTables.exercises.nonDuplicateChar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NonDuplicateCharTest {
+    @Test
+    public void shouldReturnFirstNonDuplicate() {
+        // Arrange
+        String testString = "minimum";
+
+        // Act
+        Character result = NonDuplicateChar.findFirstNonDuplicate(testString);
+
+        // Assert
+        Assertions.assertEquals('n', result);
+    }
+
+    @Test
+    public void shouldReturnFirstNonDuplicateIfContainsUppercase() {
+        // Arrange
+        String testString = "This STRING contains UPPERCASE letters";
+
+        // Act
+        Character result = NonDuplicateChar.findFirstNonDuplicate(testString);
+
+        // Assert
+        Assertions.assertEquals('h', result);
+    }
+
+    @Test
+    public void shouldReturnNullIfNoNonDuplicates() {
+        // Arrange
+        String testString = "Hello world hello World";
+
+        // Act
+        Character result = NonDuplicateChar.findFirstNonDuplicate(testString);
+
+        // Assert
+        Assertions.assertNull( result);
+    }
+}
