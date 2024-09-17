b818852d2818fad0dc61022c74a778cdb313dbfe
Tom Bolton <tbolton96@icloud.com>
2024-09-17 20:32:01 +0100

    binary search tree delete method and toString method

--- src/main/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTree.java
+++ src/main/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTree.java
@@ -28,4 +28,81 @@ public class BinarySearchTree {
             }
         }
     }
+
+    public static <T> TreeNode<T> delete(T value, TreeNode<T> node) {
+        if (node == null) {
+            return null;
+        } else if ((Integer) value < (Integer) node.getValue()) {
+            node.setLeftChild(delete(value, node.getLeftChild()));
+            return node;
+        } else if ((Integer) value > (Integer) node.getValue()) {
+            node.setRightChild(delete(value, node.getRightChild()));
+            return node;
+        } else if (value == node.getValue()) {
+            if (node.getLeftChild() == null) {
+                return node.getRightChild();
+            } else if (node.getRightChild() == null) {
+                return node.getLeftChild();
+            } else {
+                node.setRightChild(lift(node.getRightChild(), node));
+                return node;
+            }
+        }
+        return null;
+    }
+
+    private static <T> TreeNode<T> lift(TreeNode<T> node, TreeNode<T> nodeToDelete) {
+       if (node.getLeftChild() != null) {
+           node.setLeftChild(lift(node.getLeftChild(), nodeToDelete));
+           return node;
+       } else {
+            nodeToDelete.setValue(node.getValue());
+            return node.getRightChild();
+       }
+    }
+
+    public static <T> String toString(TreeNode<T> root) {
+
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(root.getValue());
+
+        String pointerRight = "└──";
+        String pointerLeft = (root.getRightChild() != null) ? "├──" : "└──";
+
+        childNodeToString(root.getLeftChild(), stringBuilder, "", pointerLeft, root.getRightChild() != null);
+        childNodeToString(root.getRightChild(), stringBuilder, "", pointerRight, false);
+
+        return stringBuilder.toString();
+    }
+
+    private static <T> void childNodeToString(TreeNode<T> node,
+                                              StringBuilder stringBuilder,
+                                              String padding,
+                                              String pointer,
+                                              boolean hasRightSibling) {
+        if (node != null) {
+            stringBuilder.append("\n");
+            stringBuilder.append(padding);
+            stringBuilder.append(pointer);
+            stringBuilder.append(node.getValue());
+
+            StringBuilder paddingBuilder = new StringBuilder(padding);
+            if (hasRightSibling) {
+                paddingBuilder.append("│  ");
+            } else {
+                paddingBuilder.append("  ");
+            }
+
+            String paddingForBoth = paddingBuilder.toString();
+            String pointerForRight = "└──";
+            String pointerForLeft = (node.getRightChild() != null) ? "├──" : "└──";
+
+            childNodeToString(node.getLeftChild(), stringBuilder, paddingForBoth, pointerForLeft, node.getRightChild() != null);
+            childNodeToString(node.getRightChild(), stringBuilder, paddingForBoth, pointerForRight, false);
+        }
+    }
 }
--- src/test/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTreeTest.java
+++ src/test/java/dev/tombolton/dsa/trees/examples/binarysearchtree/BinarySearchTreeTest.java
@@ -87,4 +87,179 @@ public class BinarySearchTreeTest {
         assertNull(node84.getLeftChild());
         assertNull(node84.getRightChild());
     }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_noChildren() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(6, root);
+
+        // Assert
+        assertNull(node27.getLeftChild());
+    }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_leftChild() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(4);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        node6.setLeftChild(node4);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(6, root);
+
+        // Assert
+        assertEquals(4, node27.getLeftChild().getValue());
+    }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_rightChild() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+        TreeNode<Integer> node8 = new TreeNode<>(8);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        node6.setRightChild(node8);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(6, root);
+
+        // Assert
+        assertEquals(8, node27.getLeftChild().getValue());
+    }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_twoChildren_leftSide() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(4);
+        TreeNode<Integer> node8 = new TreeNode<>(8);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        node6.setLeftChild(node4);
+        node6.setRightChild(node8);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(6, root);
+
+        // Assert
+        assertEquals(8, node27.getLeftChild().getValue());
+        assertNull(node8.getLeftChild());
+    }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_twoChildren_rightSide() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(4);
+        TreeNode<Integer> node8 = new TreeNode<>(8);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        node6.setLeftChild(node4);
+        node6.setRightChild(node8);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(72, root);
+
+        // Assert
+        assertEquals(84, root.getRightChild().getValue());
+        assertEquals(53, root.getRightChild().getLeftChild().getValue());
+        assertNull(root.getRightChild().getRightChild());
+    }
+
+    @Test
+    public void deleteShouldDeleteNodeFromTree_rootNode() {
+        // Arrange
+        TreeNode<Integer> node72 = new TreeNode<>(72);
+        TreeNode<Integer> node48 = new TreeNode<>(48);
+        TreeNode<Integer> node84 = new TreeNode<>(84);
+        TreeNode<Integer> node53 = new TreeNode<>(53);
+        TreeNode<Integer> node27 = new TreeNode<>(27);
+        TreeNode<Integer> node6 = new TreeNode<>(6);
+        TreeNode<Integer> node4 = new TreeNode<>(4);
+        TreeNode<Integer> node8 = new TreeNode<>(8);
+
+        node72.setRightChild(node84);
+        node72.setLeftChild(node53);
+
+        node27.setRightChild(node48);
+        node27.setLeftChild(node6);
+
+        node6.setLeftChild(node4);
+        node6.setRightChild(node8);
+
+        TreeNode<Integer> root = new TreeNode<>(50, node27, node72);
+
+        // Act
+        BinarySearchTree.delete(50, root);
+
+        // Assert
+        assertEquals(53, root.getValue());
+        assertNull(node72.getLeftChild());
+    }
 }
